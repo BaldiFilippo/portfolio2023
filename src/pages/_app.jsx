2949f554429e78a0e744adbf71fc723f1b6db60b
Baldi Filippo <baldi.filippo@icloud.com>
2023-01-21 08:53:57 +0100

    first commit

---
+++ src/pages/_app.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from 'react'
+
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+
+import '@/styles/tailwind.css'
+import 'focus-visible'
+
+function usePrevious(value) {
+  let ref = useRef()
+
+  useEffect(() => {
+    ref.current = value
+  }, [value])
+
+  return ref.current
+}
+
+export default function App({ Component, pageProps, router }) {
+  let previousPathname = usePrevious(router.pathname)
+
+  return (
+    <>
+      <div className="fixed inset-0 flex justify-center ">
+        <div className="bg-white-100 ring-white-100 w-full ring-1 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+      </div>
+      <div className="relative">
+        <Header />
+        <main>
+          <Component previousPathname={previousPathname} {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </>
+  )
+}
