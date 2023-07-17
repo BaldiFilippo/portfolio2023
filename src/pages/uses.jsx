5907af41a8c084d0ada3e61b89bb6aa6b095f007
Baldi Filippo <baldi.filippo@icloud.com>
2023-07-17 15:47:20 +0200

    uses page

--- src/pages/uses.jsx
+++ src/pages/uses.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Head from 'next/head'
 
 import { Card } from '@/components/Card'
@@ -42,69 +43,43 @@ export default function Uses() {
       >
         <div className="space-y-20">
           <ToolsSection title="Workstation">
-            <Tool title="16” MacBook Pro, M1 Max, 64GB RAM (2021)">
-              I was using an Intel-based 16” MacBook Pro prior to this and the
-              difference is night and day. I’ve never heard the fans turn on a
-              single time, even under the incredibly heavy loads I put it
-              through with our various launch simulations.
+            <Tool title=" MacBook Air, M1 (2020)">
+              usavo un MacBook Pro da 16 pollici basato su Intel prima di
+              questo. Devo dire che il nuovo sistema su chip M1 è davvero
+              impressionante.
             </Tool>
             <Tool title="Apple Pro Display XDR (Standard Glass)">
               The only display on the market if you want something HiDPI and
               bigger than 27”. When you’re working at planetary scale, every
-              pixel you can get counts.
+              pixel you can get counts. Uso questo computer portatile come
+              dispositivo principale per le mie attività di sviluppo.
             </Tool>
-            <Tool title="IBM Model M SSK Industrial Keyboard">
-              They don’t make keyboards the way they used to. I buy these any
-              time I see them go up for sale and keep them in storage in case I
-              need parts or need to retire my main.
+            <Tool title="Apple magic keyboard">
+              Un ottimo prodotto che mi permette di scrivere in modo
+              confortevole con un design elegante.
             </Tool>
             <Tool title="Apple Magic Trackpad">
-              Something about all the gestures makes me feel like a wizard with
-              special powers. I really like feeling like a wizard with special
-              powers.
+              Prodotto che introduce delle gesture uniche che mi permettono di
+              lavorare in modo più veloce.
             </Tool>
           </ToolsSection>
           <ToolsSection title="Development tools">
             <Tool title="Arc">
-              I am very interested in the project and because it differentiates
-              itself from other browsers. It offers features such as privacy and
-              security enhancements, as well as a customizable user interface.
-              Additionally, being an open-source project, I appreciate the
-              transparency and community involvement in its development.
+              Sono molto interessato al progetto e perché si differenzia da
+              altri browser. Offre funzionalità come privacy e miglioramenti
+              della sicurezza, nonché un'interfaccia utente personalizzabile.q
+              Inoltre, essendo un progetto open source, apprezzo il trasparenza
+              e coinvolgimento della comunità nel suo sviluppo.
             </Tool>
             <Tool title="Visual Studio Code">
-              I’ve tried a lot of different editors over the years but I always
-              come back to VS Code. It’s just so fast and has so many great
-              extensions.
-            </Tool>
-            <Tool title="iTerm2">
-              I’m honestly not even sure what features I get with this that
-              aren’t just part of the macOS Terminal but it’s what I use.
+              Reputo questo editor il migliore sotto doversi punti di vista.
+              Perfetto per lo sviluppo di applicazioni web.
             </Tool>
           </ToolsSection>
           <ToolsSection title="Design">
             <Tool title="Figma">
-              We started using Figma as just a design tool but now it’s become
-              our virtual whiteboard for the entire company. Never would have
-              expected the collaboration features to be the real hook.
-            </Tool>
-          </ToolsSection>
-          <ToolsSection title="Productivity">
-            <Tool title="Reflect">
-              Using a daily notes system instead of trying to keep things
-              organized by topics has been super powerful for me. And with
-              Reflect, it’s still easy for me to keep all of that stuff
-              discoverable by topic even though all of my writing happens in the
-              daily note.
-            </Tool>
-            <Tool title="SavvyCal">
-              Great tool for scheduling meetings while protecting my calendar
-              and making sure I still have lots of time for deep work during the
-              week.
-            </Tool>
-            <Tool title="Focus">
-              Simple tool for blocking distracting websites when I need to just
-              do the work and get some momentum going.
+              Ho iniziato a utilizzare Figma solo come strumento di
+              progettazione, ma ora è diventato la lavagna virtuale presonale.
             </Tool>
           </ToolsSection>
         </div>
